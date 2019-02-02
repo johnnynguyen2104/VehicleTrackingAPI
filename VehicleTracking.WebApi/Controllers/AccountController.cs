242ac36b45fdfa8396818c84e535e60ea3c791dd
Johnny <johnny@goquo.com>
2019-02-02 13:19:04 +0800

    masked code

--- VehicleTracking.WebApi/Controllers/AccountController.cs
+++ VehicleTracking.WebApi/Controllers/AccountController.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using VehicleTracking.Application.BankAccounts.Commands;
-using VehicleTracking.Application.BankAccounts.Models;
-using VehicleTracking.Application.BankAccounts.Queries;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -22,32 +20,32 @@ namespace VehicleTracking.WebApi.Controllers
             _logger = logger;
         }
 
-        [Route("/api/account/balance/{AccountNumber}")]
-        [ProducesResponseType(typeof(IEnumerable<InquiryViewModel>), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> Inquiry(string AccountNumber)
-        {
-            InquiryByAccountNumberQuery query = new InquiryByAccountNumberQuery() { AccountNumber = AccountNumber };
-            return Ok(await Mediator.Send(query));
-        }
+        //[Route("/api/account/balance/{AccountNumber}")]
+        //[ProducesResponseType(typeof(IEnumerable<InquiryViewModel>), (int)HttpStatusCode.OK)]
+        //public async Task<IActionResult> Inquiry(string AccountNumber)
+        //{
+        //    InquiryByAccountNumberQuery query = new InquiryByAccountNumberQuery() { AccountNumber = AccountNumber };
+        //    return Ok(await Mediator.Send(query));
+        //}
 
-        [HttpPost]
-        [Route("/api/account/withdraw")]
-        [ProducesResponseType(typeof(IEnumerable<InquiryViewModel>), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> Withdraw([FromBody] WithdrawCommand command)
-        {
-            await Mediator.Send(command);
-            var query = new InquiryByAccountNumberQuery() { AccountNumber = command.AccountNumber };
-            return Ok(await Mediator.Send(query));
-        }
+        //[HttpPost]
+        //[Route("/api/account/withdraw")]
+        //[ProducesResponseType(typeof(IEnumerable<InquiryViewModel>), (int)HttpStatusCode.OK)]
+        //public async Task<IActionResult> Withdraw([FromBody] WithdrawCommand command)
+        //{
+        //    await Mediator.Send(command);
+        //    var query = new InquiryByAccountNumberQuery() { AccountNumber = command.AccountNumber };
+        //    return Ok(await Mediator.Send(query));
+        //}
 
-        [HttpPost]
-        [Route("/api/account/deposit")]
-        [ProducesResponseType(typeof(IEnumerable<InquiryViewModel>), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> Deposit([FromBody] DepositCommand command)
-        {
-            await Mediator.Send(command);
-            var query = new InquiryByAccountNumberQuery() { AccountNumber = command.AccountNumber };
-            return Ok(await Mediator.Send(query));
-        }
+        //[HttpPost]
+        //[Route("/api/account/deposit")]
+        //[ProducesResponseType(typeof(IEnumerable<InquiryViewModel>), (int)HttpStatusCode.OK)]
+        //public async Task<IActionResult> Deposit([FromBody] DepositCommand command)
+        //{
+        //    await Mediator.Send(command);
+        //    var query = new InquiryByAccountNumberQuery() { AccountNumber = command.AccountNumber };
+        //    return Ok(await Mediator.Send(query));
+        //}
     }
 }
