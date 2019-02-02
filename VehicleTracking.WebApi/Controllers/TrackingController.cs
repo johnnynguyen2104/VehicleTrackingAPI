d4fbc67ea8a8a30cd46905641196d260b8717385
Johnny <johnny@goquo.com>
2019-02-02 16:56:17 +0800

    added apis

---
+++ VehicleTracking.WebApi/Controllers/TrackingController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VehicleTracking.Application.VehicleModule.Commands;
+using VehicleTracking.Application.VehicleModule.Queries;
+
+namespace VehicleTracking.WebApi.Controllers
+{
+
+    public class TrackingController : BaseController
+    {
+
+        protected readonly ILogger<TrackingController> _logger;
+
+        public TrackingController(ILogger<TrackingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("/tracking")]
+        public async Task<IActionResult> RecordPosition(RecordPositionCommand recordCommand)
+        {
+            await Mediator.Send(recordCommand);
+            return NoContent();
+        }
+
+        [Route("/tracking/{vehicleId}/{activatedCode}/{deviceCode}")]
+        public async Task<IActionResult> GetCurrentPosition([FromQuery] CurrentPositionQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+
+        [Route("/tracking/{vehicleId}/{activatedCode}/{deviceCode}/journey/{fromDateTime}/{toDateTime}")]
+        public async Task<IActionResult> Deposit([FromQuery] JourneyQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+    }
+}
---
+++ VehicleTracking.WebApi/Controllers/VehicleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VehicleTracking.Application.VehicleModule.Commands;
+
+namespace VehicleTracking.WebApi.Controllers
+{
+    public class VehicleController : BaseController
+    {
+        protected readonly ILogger<VehicleController> _logger;
+
+        public VehicleController(ILogger<VehicleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("/vehicle")]
+        public async Task<IActionResult> RegisterVehicle([FromBody] RegisterVehicleCommand command)
+        {
+            await Mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
