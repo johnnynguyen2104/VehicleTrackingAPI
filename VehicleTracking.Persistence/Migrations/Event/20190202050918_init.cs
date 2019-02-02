ac26d145fdf82c4c43663bcf2baf43479d7974a6
Johnny <johnny@goquo.com>
2019-02-02 13:18:34 +0800

    added migrations

---
+++ VehicleTracking.Persistence/Migrations/Event/20190202050918_init.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VehicleTracking.Persistence.Migrations.Event
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TrackingPoints",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(nullable: true),
+                    Latitude = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Longitude = table.Column<string>(type: "varchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrackingPoints", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrackingPointSnapshots",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(nullable: true),
+                    VehicleReferencedCode = table.Column<string>(type: "varchar(50)", nullable: false),
+                    StartPointId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Note = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrackingPointSnapshots", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TrackingPointSnapshots_TrackingPoints_StartPointId",
+                        column: x => x.StartPointId,
+                        principalTable: "TrackingPoints",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrackingPointSnapshots_StartPointId",
+                table: "TrackingPointSnapshots",
+                column: "StartPointId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrackingPointSnapshots");
+
+            migrationBuilder.DropTable(
+                name: "TrackingPoints");
+        }
+    }
+}
---
+++ VehicleTracking.Persistence/Migrations/Vehicle/20190202050704_init.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VehicleTracking.Persistence.Migrations.Vehicle
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditLogs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(nullable: true),
+                    EntitiesName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    UpdatedData = table.Column<string>(type: "varchar(max)", nullable: true),
+                    OldData = table.Column<string>(type: "varchar(max)", nullable: true),
+                    EntityId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Action = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(nullable: true),
+                    DeviceCode = table.Column<string>(type: "varchar(20)", nullable: false),
+                    ActivatedCode = table.Column<string>(type: "varchar(20)", nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    RegisteredName = table.Column<string>(nullable: false),
+                    RegisteredPhone = table.Column<string>(type: "varchar(50)", nullable: false),
+                    DeviceModel = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicle_ActivatedCode",
+                table: "Vehicle",
+                column: "ActivatedCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicle_DeviceCode",
+                table: "Vehicle",
+                column: "DeviceCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicle_RegisteredPhone",
+                table: "Vehicle",
+                column: "RegisteredPhone",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditLogs");
+
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
