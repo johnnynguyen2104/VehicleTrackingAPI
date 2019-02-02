63f575885366dd6fded078a7ba22683a56f1703d
Johnny <johnny@goquo.com>
2019-02-02 16:56:08 +0800

    added error details

---
+++ VehicleTracking.Common/ErrorDetails.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleTracking.Common
+{
+    public class ErrorDetails
+    {
+        public string ErrorCode { get; set; }
+
+        public string Reason { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
