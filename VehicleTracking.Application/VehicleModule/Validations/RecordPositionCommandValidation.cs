04e7ac2a32f3d82f952b19df100351a3efb27cd2
Johnny <johnny@goquo.com>
2019-02-03 17:32:13 +0800

    added validations

--- VehicleTracking.Application/VehicleModule/Validations/RecordPositionCommandValidation.cs
+++ VehicleTracking.Application/VehicleModule/Validations/RecordPositionCommandValidation.cs
@@ -21,6 +21,18 @@ namespace VehicleTracking.Application.VehicleModule.Validations
             RuleFor(a => a.ActivatedCode)
                 .NotNull()
                 .NotEmpty();
+
+            RuleFor(a => a.Latitude)
+               .NotNull()
+               .NotEmpty()
+               .Matches(@"^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$")
+               .WithMessage("Invalid Latitude.");
+
+            RuleFor(a => a.Longitude)
+              .NotNull()
+              .NotEmpty()
+              .Matches(@"^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$")
+              .WithMessage("Invalid Longitude."); ;
         }
     }
 }
