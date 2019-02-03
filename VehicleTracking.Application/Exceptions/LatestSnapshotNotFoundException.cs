55b05159989becc507849ae552188e99e829725c
Johnny <johnny@goquo.com>
2019-02-03 14:32:07 +0800

    added new exception

---
+++ VehicleTracking.Application/Exceptions/LatestSnapshotNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleTracking.Application.Exceptions
+{
+    public class LatestSnapshotNotFoundException : Exception
+    {
+        public LatestSnapshotNotFoundException(string message)
+            :base(message)
+        {
+
+        }
+    }
+}
