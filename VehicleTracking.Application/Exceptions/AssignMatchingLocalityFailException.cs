0059f05da2574e2885a2bd41ac5f7909533c40f5
Johnny <johnny@goquo.com>
2019-02-03 15:50:02 +0800

    new exception

---
+++ VehicleTracking.Application/Exceptions/AssignMatchingLocalityFailException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleTracking.Application.Exceptions
+{
+    public class AssignMatchingLocalityFailException : Exception
+    {
+        public AssignMatchingLocalityFailException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
